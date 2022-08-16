e5e9a7a734cd4122569ecf6166b7385ed9b7b7a6
Igor Sokolov <igor.sokolov@mailfence.com>
2022-08-16 22:41:05 +0200

    Made better start for table layout

--- clx-table.lisp
+++ clx-table.lisp
@@ -6,52 +6,74 @@
          (colormap (screen-default-colormap screen)))
     (values display screen colormap)))
 
-(defun make-x11-layers (screen window-size colormap x-start plot-window-size)
-  "Create main windows and gcontexts"
-  (let* ((main-window (create-window
-		       :parent (screen-root screen)
-		       :x 0
-		       :y 0
-		       :width window-size
-		       :height window-size
-		       :border (screen-black-pixel screen)
-		       :border-width 2
-		       :bit-gravity :center
-		       :colormap colormap
-		       :background (alloc-color colormap (lookup-color colormap "white"))))
-	 (plot-window (create-window
-		       :parent main-window
-		       :x x-start
-		       :y 50
-		       :width plot-window-size
-		       :height plot-window-size
-		       :border (screen-black-pixel screen)
-		       :border-width 2
-		       :colormap colormap
-		       :background (alloc-color colormap (lookup-color colormap "green"))))	 
-         (grid (create-gcontext
-                :drawable plot-window
-		:line-style :solid
-                :background (screen-white-pixel screen)
-                :foreground (alloc-color colormap (lookup-color colormap "orange")))))    
+(defstruct table-layout
+  table-window)
+
+(defun create-table-layout (main-window screen colormap x-start y-start x-size y-size)
+  (let ((tl (make-table-layout
+	     :table-window (create-window
+			    :parent main-window
+			    :x x-start
+			    :y y-start
+			    :width x-size
+			    :height y-size
+			    :border (screen-black-pixel screen)
+			    :border-width 2
+			    :colormap colormap
+			    :background (alloc-color colormap (lookup-color colormap "green"))))))
     (map-window main-window)
-    (map-window plot-window)
-    (values main-window plot-window grid)))
+    (map-window (table-layout-table-window tl))))
 
-(defun make-plot-window (size start-offest-in-% end-offset-in-%) ;; offsets are in % of window-size
-  "Set plot-window size"
-  (let* ((x-start (round (* start-offest-in-% size)))
-	 (x-end (round (* end-offset-in-% size)))
-	 (plot-window-size (- x-end x-start)))
-    (values size x-start x-end plot-window-size)))
+;; (defun make-x11-layers (screen window-size colormap x-start plot-window-size)
+;;   "Create main windows and gcontexts"
+;;   (let* ((main-window (create-window ;; must be inhereted
+;; 		       :parent (screen-root screen)
+;; 		       :x 0
+;; 		       :y 0
+;; 		       :width window-size
+;; 		       :height window-size
+;; 		       :border (screen-black-pixel screen)
+;; 		       :border-width 2
+;; 		       :bit-gravity :center
+;; 		       :colormap colormap
+;; 		       :background (alloc-color colormap (lookup-color colormap "white"))))
+;; 	 (plot-window (create-window
+;; 		       :parent main-window
+;; 		       :x x-start
+;; 		       :y 50
+;; 		       :width plot-window-size
+;; 		       :height plot-window-size
+;; 		       :border (screen-black-pixel screen)
+;; 		       :border-width 2
+;; 		       :colormap colormap
+;; 		       :background (alloc-color colormap (lookup-color colormap "green"))))	 
+;;          (grid (create-gcontext
+;;                 :drawable plot-window
+;; 		:line-style :solid
+;;                 :background (screen-white-pixel screen)
+;;                 :foreground (alloc-color colormap (lookup-color colormap "orange")))))    
+;;     (map-window main-window)
+;;     (map-window plot-window)
+;;     (values main-window plot-window grid)))
 
-(Defun test ()
+
+(defun test ()
   (multiple-value-bind (display screen colormap) (make-default-display-screen-colormap)
-    (multiple-value-bind (window-size x-start x-end plot-window-size) (make-plot-window 1500 0.6 0.9)
-      (multiple-value-bind (main-window plot-window grid) (make-x11-layers screen window-size colormap x-start plot-window-size)
-	(unwind-protect
-	     (display-force-output display)
-	  (sleep 3)
+    (let ((main-window (create-window
+			 :parent (screen-root screen)
+			 :x 0
+			 :y 0
+			 :width 1500
+			 :height 1500
+			 :border (screen-black-pixel screen)
+			 :border-width 2
+			 :bit-gravity :center
+			 :colormap colormap
+			 :background (alloc-color colormap (lookup-color colormap "white")))))
+      (create-table-layout main-window screen colormap 800 50 200 500)
+      (unwind-protect
+	   (display-force-output display)
+	(sleep 3)
 	(display-finish-output display)
-	(close-display display))))))
+	(close-display display)))))
 	
